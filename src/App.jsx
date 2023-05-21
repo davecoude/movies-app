3ff057fc923e10fba2082bbc9b01678665e60500
Diego Vanegas <diegoalejandrovanegas97@gmail.com>
2023-05-21 18:53:30 -0500

    Costum hook para la barra de busqueda

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,62 @@
 import './App.css'
 import { Movies } from './components/Movies'
 import { useMovie } from './hooks/useMovie'
+import { useEffect, useRef, useState } from 'react'
+
+function useSearch () {
+  const [search, setSearch] = useState('')
+  const [error, setError] = useState(null)
+  const isFirstTime = useRef(true)
+
+  useEffect(() => {
+    
+    if (isFirstTime.current) {
+      isFirstTime.current = search === ''
+      return
+    }
+
+    if (search === '') {
+      setError('La barra de busqueda no debe estar vacia')
+      return
+    }
+
+    if(search.length < 3) {
+      setError('La busqueda debe contener mas de 3 caracteres')
+      return
+    }
+
+    setError(null)
+
+  }, [search])
+
+  return { search, setSearch, error }
+}
 
 export default function App () {
  const { movies } = useMovie()
+ const { search, setSearch, error } = useSearch()
+
+ const handleSubmit = (event) => {
+  event.preventDefault()
+ }
+
+ const handleChange = (event) => { 
+  setSearch(event.target.value)
+ }
+
+ console.log(search)
  
   return (
     <div className='page'>
       <header>
         <h1>Buscador de peliculas</h1>
-        <form action="" className='form'>
-          <input placeholder="Avengers, Star wars, The matrix" />
+        <form action="" onSubmit={handleSubmit} className='form'>
+
+          <input onChange={handleChange} value={search} name='movie1' placeholder="Avengers, Star wars, The matrix" />
           <button type='submit'>Buscar</button>
+
         </form>
+        {error && <p style={{color: 'red'}}>{error}</p>}
       </header>
 
       <main>
