a82b87a95686ffa06b1c6f22024b1ef61ee8e396
Diego Vanegas <diegoalejandrovanegas97@gmail.com>
2023-05-23 21:49:24 -0500

    Separación logica fetching de datos

---
+++ src/services/movies.js
@@ -0,0 +1,22 @@
+const API_KEY = 'b39a67bf'
+
+export const searchMovies = async ({ search }) => {
+  if (search === '') return null
+
+  try {
+    const response = await fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`)
+    const json = await response.json()
+
+    const movies = json.Search
+    // evitar contrato con la API directamente en la UI
+    return movies?.map(movie => ({
+      id: movie.imdbID,
+      title: movie.Title,
+      type: movie.Type,
+      poster: movie.Poster
+    }))
+
+  } catch (e) {
+    throw new Error('Error Searching Movie')
+  }
+}
