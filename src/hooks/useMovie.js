cda892da403a119ac1c09e9b80bba418e5ca53c1
Diego Vanegas <diegoalejandrovanegas97@gmail.com>
2023-05-25 22:32:19 -0500

    Estado para loading, ordenar peliculas, evitar misma busqueda

--- src/hooks/useMovie.js
+++ src/hooks/useMovie.js
@@ -1,13 +1,38 @@
-import { useState } from 'react'
+import { useMemo, useRef, useState } from 'react'
 import { searchMovies } from '../services/movies.js'
 
-export function useMovie({ search }) {
+export function useMovie({ search, sort }) {
   const [movies, setMovies] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const previousSearch = useRef(search)
 
   const getMovies = async () => {
-    const newMovies = await searchMovies({ search })
-    setMovies(newMovies)
+    if ( search === previousSearch.current) return
+
+    try {
+      setLoading(true)
+      setError(null)
+      previousSearch.current = search
+      const newMovies = await searchMovies({ search })
+      setMovies(newMovies)
+
+    } catch (e) {
+      setError(e.message)
+
+    } finally {
+      setLoading(false)
+
+    }
   }
 
-  return { movies, getMovies}
+  const sortedMovies = useMemo(() => {
+    console.log('sorted')
+    return sort
+      ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
+      : movies
+  }, [sort, movies])
+
+
+  return { movies: sortedMovies, getMovies, loading}
 }
